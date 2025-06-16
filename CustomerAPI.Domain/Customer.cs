03c8b7c6ce02834fb7d166e7b9c8540defbd050e
zahidmustafa@outlook.com <zahidmustafa@outlook.com>
2025-06-16 22:05:41 +0500

    Changes

---
+++ CustomerAPI.Domain/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Domain
+{
+    public class Customer
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string? MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+    }
+}

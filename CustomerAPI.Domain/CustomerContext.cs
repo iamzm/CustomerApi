f3ab099334076811f1fd7066fee4c5a0795ae60b
zahidmustafa@outlook.com <zahidmustafa@outlook.com>
2025-06-16 21:32:19 +0500

    Add project files.

---
+++ CustomerAPI.Domain/CustomerContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Domain
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options)
+            : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasIndex(c => c.Email)
+                .IsUnique();
+        }
+    }
+}
